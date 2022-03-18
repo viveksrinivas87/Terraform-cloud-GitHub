51920997ef19a6508ca0e56301a2ebe6161adcf7
viveksrinivas87 <92803662+viveksrinivas87@users.noreply.github.com>
2022-03-18 19:15:40 +0530

    Updated as per Vivek Login

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.0" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,10 +10,10 @@ terraform {
 
 provider "aws" {
        # region = "us-east-1" 
-        profile = "default"
+       # profile = "default"
 	}
 
 resource "aws_instance" "Raminfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
+	ami = "ami-0c02fb55956c7d316" 
 	instance_type = "t2.micro"
 }
